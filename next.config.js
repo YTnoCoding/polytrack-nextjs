20edb7a12568d20fd41d47c99d102c2529af1ac0
Kevin <kevin@Kevindebijibendiannao.local>
2024-11-19 12:13:51 +0800

    update config

--- next.config.js
+++ next.config.js
@@ -1,17 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: [''],
-  },
-  // 禁用所有页面的预加载
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  experimental: {
-    // 禁用预加载
-    prefetchRegex: false,
-  },
+  reactStrictMode: true
 }
 
 module.exports = nextConfig
